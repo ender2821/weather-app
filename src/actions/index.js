c012866e7d62e1c19e442ee31542ec5c66beeec7
Josh Jensen <ender2821@yahoo.com>
2018-02-17 15:32:08 -0600

    hooked up API data fetching to the searchbar container

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_KEY = 'eaba41413d6e0956cd2228b2e985c2dd';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}` ;
+
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+
+
+	return{
+		type: FETCH_WEATHER, 
+		payload: request
+	};
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 
 
-const API_KEY = 'eaba41413d6e0956cd2228b2e985c2dd';
 
 const rootReducer = combineReducers({
   state: (state = {}) => state
