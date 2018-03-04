655b663a96f3f11451aaca8d3b5c1cb6d19913a8
Joshua Jensen <ender2821@yahoo.com>
2018-03-03 23:48:06 -0600

    fixed issue with double export on reducer. Data now loads successfully. Also added redux devtool

--- src/reducers/reducer-weather.js
+++ src/reducers/reducer-weather.js
@@ -1,10 +1,14 @@
-import FETCH_WEATHER from '../actions/index';
+import { FETCH_WEATHER } from '../actions/index';
 
 export default function(state = [], action) {
 	console.log('Action Recieved', action);
+	// debugger
+	if(action.payload && action.payload.data) {
+		debugger
+	}
 	switch(action.type) {
 	case FETCH_WEATHER: 
-
+		console.log("FETCH_WEATHER")
 		// NEVER DO THIS!!!! - Mutate State. " state.push(action.payload.data) "
 
 		// return state.concat([action.payload.data]); ES6 variant below
